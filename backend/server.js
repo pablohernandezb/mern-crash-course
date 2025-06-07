6669a4907c918d1d21aa46e7bd44d007054868e5
Pablo Hernandez Borges <pablo.hernandez.borges@gmail.com>
2025-06-07 14:26:46 -0400

    Fixing server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json()); // Middleware to parse JSON bodies
 
 app.use("/api/products", productRoutes);
 
-if (process.env.NODE_ENV === "production ") {
+if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 	app.get(/(.*)/, (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
