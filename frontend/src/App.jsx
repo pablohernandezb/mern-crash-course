e6beb48d9d054d4de2d5421d515194956fa7ef62
Pablo Hernandez Borges <pablo.hernandez.borges@gmail.com>
2025-06-06 23:59:25 -0400

    Fixed a spelling error on App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Box } from '@chakra-ui/react';
 import { Routes, Route } from 'react-router-dom';
 
-import HomePage from './pages/Homepage';
+import HomePage from './pages/HomePage';
 import CreatePage from './pages/CreatePage';  
 import Navbar from './components/Navbar';
 
